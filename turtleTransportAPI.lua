3bb21d7db8a62b3eedb3e8e120e2f29f0e27c283
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2018-01-15 17:31:35 +0000

    new plan

--- turtleTransportAPI.lua
+++ turtleTransportAPI.lua
@@ -3,45 +3,21 @@
 
 local api = shell and {} or (_ENV or getfenv())
 
-api.modemChannel = 65270 -- I just picked a random number above 65000 but below the max, hopefully this is unique.
+api.PROTOCOL_NAME = "turtleTransportationFramework"
 
-api.modem = false -- If you want to override the communication method (redstone perhaps?) then set this to a table which mimics a wireless modem, don't forget to open the modem channel (or make a stub function which mimics this).
-
-function api.setUpModem()
-  if not api.modem then
-    api.modem = peripheral.find("modem", function(name, object) return object.isWireless() end )
-  end
-  api.modem.open(api.modemChannel)
-end
-
-local function checkForModem()
-  if not api.modem  then
-    error("API not assigned a modem, use api.setUpModem()")
-  end
-end
+-- Transport Station API
+-- This is the API for computers which run the stations where turtles 'board' the transports.
+api.transportStationAPI = {}
+local transportStationAPI = api.transportStationAPI -- cache for internal use
 
 
-function api.getListOfDestinations()
-  checkForModem()
-end
 
-function api.setDestination(destinationName)
-  checkForModem()
-end
 
-function api.requestBoarding()
-  checkForModem()
-end
+-- Turtle API
+-- This is the API for the turtles which will be transported using the system.
+api.turtleAPI = {}
+local turtleAPI = api.turtleAPI -- cache for internal use
 
-function api.filterEvent(...)
-  local args = table.pack(...)
-  if event[1] == "modem_message" then
-    local frequency = event[2]
-    local returnFrequency = event[3]
-    local message = event[4]
-    local distance = event[5]
-    
-end
 
 
 
